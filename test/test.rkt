cdb15fdebc3f899862842e2d2f3da0208e30cf87
Antigen-1 <zhanghao@antigen.top>
2023-03-23 18:32:40 +0800

    add a test

---
+++ test/test.rkt
@@ -0,0 +1,8 @@
+#lang reader "../main.rkt"
+(require racket/unit)
+
+(check-equal? ''[v for v in (range 0 1000) if (zero? (remainder v 7))]
+              (let loop ((s 0) (r null))
+                (define v (* s 7))
+                (cond ((>= v 1000) (reverse r))
+                      (else (loop (add1 s) (cons v r))))))
